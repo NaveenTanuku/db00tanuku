d086f92eeb53d05662132f60a4d3ce92d9a97e1c
NaveenTanuku <ntanuku96@gmail.com>
2021-04-14 21:06:57 -0500

    added delete code in controller

--- controllers/ball.js
+++ controllers/ball.js
@@ -9,10 +9,10 @@ exports.ball_list = async function(req, res) {
     res.error(500,`{"error": ${err}}`);
     };
 };
-// for a specific ball.
-exports.ball_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: ball detail: ' + req.params.id);
-};
+// // for a specific ball.
+// exports.ball_detail = function(req, res) {
+// res.send('NOT IMPLEMENTED: ball detail: ' + req.params.id);
+// };
 // Handle ball create on POST.
 exports.ball_create_post = async function(req, res) {
     console.log(req.body)
@@ -20,7 +20,7 @@ exports.ball_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costumetype":"goat", "cost":12, "size":"large"}
+    // {"balltype":"goat", "cost":12, "size":"large"}
     document.type = req.body.type;
     document.shape = req.body.shape;
     document.quantity = req.body.quantity;
@@ -33,13 +33,39 @@ exports.ball_create_post = async function(req, res) {
     };
 };
 // Handle ball delete form on DELETE.
-exports.ball_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: ball delete DELETE ' + req.params.id);
+exports.ball_delete = async function(req, res) {
+    console.log("delete "  + req.params.id)
+    try {
+        result = await ball.findByIdAndDelete( req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+
 };
+// // Handle ball update form on PUT.
+// exports.ball_update_put = function(req, res) {
+// res.send('NOT IMPLEMENTED: ball update PUT' + req.params.id);
+// };
 // Handle ball update form on PUT.
-exports.ball_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: ball update PUT' + req.params.id);
-};
+exports.ball_update_put = async function(req, res) {   
+     console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`) 
+     try {       
+         let toUpdate = await ball.findById( req.params.id)        
+    // Do updates of properties        
+    if(req.body.type) toUpdate.type = req.body.type;        
+    if(req.body.shape) toUpdate.shape = req.body.shape;        
+    if(req.body.quantity) toUpdate.quantity = req.body.quantity;        
+    let result = await toUpdate.save();        
+    console.log("Sucess " + result)        
+    res.send(result)    
+} catch (err) {       
+     res.status(500)        
+     res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);   
+     }};
+
 // VIEWS
 // Handle a show all view
 exports.ball_view_all_Page = async function(req, res) {
@@ -51,3 +77,15 @@ exports.ball_view_all_Page = async function(req, res) {
     res.error(500,`{"error": ${err}}`);
     }
     };
+
+        // for a specific ball.
+exports.ball_detail = async function(req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await ball.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
--- routes/ball.js
+++ routes/ball.js
@@ -5,5 +5,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', ball_controllers.ball_view_all_Page);
 
+/* GET detail ball page */
+router.get('/detail', ball_controlers.ball_view_one_Page);
+
+
 module.exports = router;
 
